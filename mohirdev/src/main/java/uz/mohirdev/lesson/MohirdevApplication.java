e5d7f3bd531794902caf455990f54ac2ed1391f8
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-03-30 21:03:17 +0500

    first commit

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/MohirdevApplication.java
@@ -0,0 +1,13 @@
+package uz.mohirdev.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohirdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohirdevApplication.class, args);
+	}
+
+}
---
+++ mohirdev/src/test/java/uz/mohirdev/lesson/MohirdevApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mohirdev.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohirdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
