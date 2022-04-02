0ca7108cd8c430372268996d8984c359b90d037a
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-04-02 23:14:10 +0500

    Student add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/model/Student.java
@@ -0,0 +1,51 @@
+package uz.mohirdev.lesson.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student {
+    private Long id;
+    private String name;
+
+    @JsonProperty("last_name")
+    private String lastName;
+    private int age;
+
+    public Student(Long id, String name, String lastName, int age) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
