ff995f8948dc828bf1df048e6f6c54fc8e8c864b
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-04-03 18:28:21 +0500

    Course class add

--- mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/StudentResource.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/StudentResource.java
@@ -2,6 +2,7 @@ package uz.mohirdev.lesson.web.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uz.mohirdev.lesson.model.Course;
 import uz.mohirdev.lesson.model.Student;
 
 import java.util.ArrayList;
@@ -43,11 +44,11 @@ public class StudentResource {
         return ResponseEntity.ok(student);
     }
 
-    @GetMapping("/students/{name}")
-    public ResponseEntity getOne(@PathVariable String name){
-        Student student = new Student(1L, name, "ikkinchi talaba", 20);
-        return ResponseEntity.ok(student);
-    }
+//    @GetMapping("/students/{name}")
+//    public ResponseEntity getOne(@PathVariable String name){
+//        Student student = new Student(1L, name, "ikkinchi talaba", 20);
+//        return ResponseEntity.ok(student);
+//    }
 
     @GetMapping("/students")
     public ResponseEntity getAll(@RequestParam Long id,
@@ -55,16 +56,26 @@ public class StudentResource {
                                  @RequestParam String lastName,
                                  @RequestParam Integer age){
         List<Student> students = new ArrayList<>();
-        students.add(new Student(id, name, lastName, age));
-        students.add(new Student(1L, "Sardor1", "Uktamov1", 29));
-        students.add(new Student(2L, "Sardor2", "Uktamov2", 30));
-        students.add(new Student(3L, "Sardor3", "Uktamov3", 28));
+        Course course = new Course();
+        course.setId(6L);
+        course.setName("Test Course");
+
+        students.add(new Student(id, name, lastName, course, age));
+        students.add(new Student(1L, "Sardor1", "Uktamov1", course, 26));
+        students.add(new Student(2L, "Sardor2", "Uktamov2",  course, 29));
+        students.add(new Student(3L, "Sardor3", "Uktamov3",  course,28));
         return  ResponseEntity.ok(students);
     }
 
     @DeleteMapping("/students/{id}/delete")
     public ResponseEntity delete(@PathVariable Long id){
         return ResponseEntity.ok(id+"-malumot uchirildi!");
+    }
+
+    @PatchMapping ("/students/{id}")
+    public ResponseEntity patchUpdate(@PathVariable Long id, @RequestBody Student student){
+//        Student student = new Student(1L, name, "ikkinchi talaba", 20);
+        return ResponseEntity.ok(student);
     }
 }
 
