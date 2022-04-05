b5ca0599c273b2dae225457e30f55cbc956a6102
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-04-05 21:48:49 +0500

    Student table add

--- mohirdev/src/main/java/uz/mohirdev/lesson/entity/Student.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/Student.java
@@ -20,4 +20,43 @@ public class Student implements Serializable {
     private String email;
     private String address;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
