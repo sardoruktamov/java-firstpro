e0964229964817579df6844f5df2862b1ed87806
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-07 15:59:06 +0500

    4 EmployeeResource add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/EmployeeResource.java
@@ -0,0 +1,27 @@
+package uz.mohirdev.lesson.web.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.mohirdev.lesson.entity.Employee;
+import uz.mohirdev.lesson.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeResource {
+
+    private final EmployeeService employeeService;
+
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity create(@RequestBody Employee employee){
+        Employee result = employeeService.save(employee);
+        return ResponseEntity.ok(result);
+    }
+}
