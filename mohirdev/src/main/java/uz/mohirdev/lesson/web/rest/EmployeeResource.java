f75f8172d1a7c580eb43f7635f04fa831ee2e8cd
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-07 17:56:30 +0500

    EmployeeResource

--- mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/EmployeeResource.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/EmployeeResource.java
@@ -1,10 +1,7 @@
 package uz.mohirdev.lesson.web.rest;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.mohirdev.lesson.entity.Employee;
 import uz.mohirdev.lesson.service.EmployeeService;
 
