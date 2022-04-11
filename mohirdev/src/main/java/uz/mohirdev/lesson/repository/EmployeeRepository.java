8830c706092fccd4853912e4c3cb7e50e93570c7
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-04-11 20:58:56 +0500

    @Query add

--- mohirdev/src/main/java/uz/mohirdev/lesson/repository/EmployeeRepository.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/repository/EmployeeRepository.java
@@ -1,10 +1,16 @@
 package uz.mohirdev.lesson.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uz.mohirdev.lesson.entity.Employee;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+    // findAll() methodini qo`lda yozamiz va shart qoyamiz
+    @Query("select e from Employee e where e.id = 5")
+    List<Employee> findAll();
 }
