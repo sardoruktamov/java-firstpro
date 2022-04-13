73f1e1a9cdf2a337e6a452fdb6fdee7d96a9cd7a
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-13 15:24:11 +0500

    @Param("name") String name

--- mohirdev/src/main/java/uz/mohirdev/lesson/repository/EmployeeRepository.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package uz.mohirdev.lesson.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uz.mohirdev.lesson.entity.Employee;
 
@@ -11,6 +12,6 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     // findAll() methodini qo`lda yozamiz va shart qoyamiz
-    @Query("select e from Employee e where e.id = 5")
-    List<Employee> findAll();
+    @Query("select e from Employee e where e.name = :name")
+    List<Employee> findAll(@Param("name") String name);
 }
