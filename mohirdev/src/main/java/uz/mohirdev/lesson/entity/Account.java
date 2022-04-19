dae12e2960dc081d9e2c6e62645bd0da8735e2fd
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-19 16:12:50 +0500

    add

--- mohirdev/src/main/java/uz/mohirdev/lesson/entity/Account.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/Account.java
@@ -1,6 +1,32 @@
 package uz.mohirdev.lesson.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
-public class Account {
+@Entity
+public class Account implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String accountNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
