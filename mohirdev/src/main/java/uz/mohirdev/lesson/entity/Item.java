42c0e38b326fb94d096589ca5fdefdaf02fafb1a
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-21 15:33:20 +0500

    Item add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/Item.java
@@ -0,0 +1,16 @@
+package uz.mohirdev.lesson.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Item implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
