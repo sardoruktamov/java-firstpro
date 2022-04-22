92e9f008f8e0c20d6b7a0c9bb9a38eb90d81f09f
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-22 17:23:27 +0500

    AccountRepository add

--- mohirdev/src/main/java/uz/mohirdev/lesson/repository/AccountRepository.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package uz.mohirdev.lesson.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uz.mohirdev.lesson.entity.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
