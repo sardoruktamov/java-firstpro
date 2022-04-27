94ecd76bc267793ceb98f45c8e2bfd51ec33984b
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-27 17:52:37 +0500

    ItemService  getID

--- mohirdev/src/main/java/uz/mohirdev/lesson/service/ItemService.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/service/ItemService.java
@@ -2,8 +2,12 @@ package uz.mohirdev.lesson.service;
 
 import org.springframework.stereotype.Service;
 import uz.mohirdev.lesson.entity.Account;
+import uz.mohirdev.lesson.entity.Employee;
 import uz.mohirdev.lesson.entity.Item;
 import uz.mohirdev.lesson.repository.ItemRepository;
+
+import java.util.Optional;
+
 
 @Service
 public class ItemService {
@@ -16,5 +20,13 @@ public class ItemService {
 
     public Item save(Item item){
         return itemRepository.save(item);
+    }
+
+    public Item findById(Long id){
+        Optional<Item> optional = itemRepository.findById(id);
+        if (optional.isPresent()){
+            return optional.get();
+        }
+        return null;
     }
 }
