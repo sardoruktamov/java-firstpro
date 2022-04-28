1404c3fec9ba63ff3377260ad44801956523934d
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-04-28 21:11:59 +0500

    Project add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/Project.java
@@ -0,0 +1,42 @@
+package uz.mohirdev.lesson.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+public class Project implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Instant createdDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+}
