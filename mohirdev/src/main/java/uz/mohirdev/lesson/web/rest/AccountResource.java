99a300b0a966188de104218b37f65fbd401188dd
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-04-29 18:02:25 +0500

    PutMapping

--- mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/AccountResource.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/AccountResource.java
@@ -3,6 +3,7 @@ package uz.mohirdev.lesson.web.rest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.mohirdev.lesson.entity.Account;
+import uz.mohirdev.lesson.entity.Item;
 import uz.mohirdev.lesson.service.AccountService;
 
 @RestController
@@ -19,6 +20,15 @@ public class AccountResource {
     public ResponseEntity create(@RequestBody Account account){
         Account result = accountService.save(account);
         return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/accounts")
+    public ResponseEntity update(@RequestBody Account account){
+        if (account.getId() == null){
+            return ResponseEntity.badRequest().build();
+        }
+        Account resoult = accountService.save(account);
+        return ResponseEntity.ok(resoult);
     }
 
     @DeleteMapping("/accounts/{id}")
