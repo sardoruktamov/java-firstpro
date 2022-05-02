d07f3c15f3f7c78b175afb2de113c41418e7d905
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-02 22:00:30 +0500

    fileStorageService change

--- mohirdev/src/main/java/uz/mohirdev/lesson/service/FileStorageService.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/service/FileStorageService.java
@@ -1,7 +1,9 @@
 package uz.mohirdev.lesson.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import uz.mohirdev.lesson.entity.FileStorage;
+import uz.mohirdev.lesson.entity.enummration.FileStorageStatus;
 import uz.mohirdev.lesson.repository.FileStorageRepository;
 
 @Service
@@ -12,7 +14,11 @@ public class FileStorageService {
         this.fileStorageRepository = fileStorageRepository;
     }
 
-    public FileStorage save(){
-        
+    public FileStorage save(MultipartFile multipartFile){
+        FileStorage fileStorage = new FileStorage();
+        fileStorage.setName(multipartFile.getOriginalFilename());
+        fileStorage.setFileSize(multipartFile.getSize());
+        fileStorage.setContentType(multipartFile.getContentType());
+        fileStorage.setFileStorageStatus(FileStorageStatus.DRAFT);
     }
 }
