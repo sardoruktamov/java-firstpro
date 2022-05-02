9267427c87fe9d4929ea949a8d17b22a6bade39c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-02 20:55:48 +0500

    FileStorageRepository

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/repository/FileStorageRepository.java
@@ -0,0 +1,8 @@
+package uz.mohirdev.lesson.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.mohirdev.lesson.entity.FileStorage;
+@Repository
+public interface FileStorageRepository extends JpaRepository<FileStorage, Long> {
+}
