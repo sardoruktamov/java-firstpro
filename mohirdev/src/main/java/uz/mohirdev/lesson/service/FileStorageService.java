f1de13f5ab3c03f47d103f4ca1e193d3befd2ad1
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-05-05 18:04:05 +0500

    .

--- mohirdev/src/main/java/uz/mohirdev/lesson/service/FileStorageService.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/service/FileStorageService.java
@@ -7,11 +7,14 @@ import uz.mohirdev.lesson.entity.FileStorage;
 import uz.mohirdev.lesson.entity.enummration.FileStorageStatus;
 import uz.mohirdev.lesson.repository.FileStorageRepository;
 
+import java.io.File;
+import java.util.Date;
+
 @Service
 public class FileStorageService {
     private final FileStorageRepository fileStorageRepository;
 
-    // applecation.yml faylidagi upload malumotini qaytaradi
+    // applecation.yml faylidagi upload malumotini qaytaradi (BU STATIC PATH)
     @Value("${upload.server.folder}")   // serverFolderPath = /Users/DXM-HP/Documents/upload/
     private String serverFolderPath;
 
@@ -29,7 +32,22 @@ public class FileStorageService {
         fileStorage = fileStorageRepository.save(fileStorage);
 
         //bu yerda serverFolderPath-static path; upload_folder-dinamic path
-        // /serverFolderPath/upload_folder/2022/04/24/dsfsdvsd.pdf
+        // /serverFolderPath/upload_files/2022/04/24/dsfsdvsd.pdf
+        // ESLATMAAAA  Date() funksiyasi 1900 yildan boshlanadi, shuning uchun 1900 yilni qoshib qoyamiz
+        // ESLATMAAAA  Date() funksiyasida oy boshi 0 dan boshlanadi, shuning uchun 1 ni qoshib qoyamiz
+
+        Date now = new Date();  // bu javadagi tayyor class xozirgi vaqtni qaytaradi
+        //formatlashning 1-usuli
+//        File uploadFolder = new File(this.serverFolderPath + "/upload_files" +
+//                1900+now.getYear() + "/" + 1 + now.getMonth() + "/" +now.getDate());
+        //formatni yozishning 2-usuli
+        String path = String.format("%s/upload_files/%d/%d/%d", this.serverFolderPath, 1900+now.getYear(),
+                1 + now.getMonth(),now.getDate());
+        File uploadFolder = new File(path);
+        if(!uploadFolder.exists() && uploadFolder.mkdir()){
+            System.out.println("Created folred");
+        }
+
         return fileStorage;
     }
 
