5cf84896f293309ac73a6d9f3abeea77b14be26a
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-05-06 18:15:08 +0500

    change

--- mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/FileStorageResource.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/FileStorageResource.java
@@ -1,4 +1,27 @@
 package uz.mohirdev.lesson.web.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import uz.mohirdev.lesson.entity.FileStorage;
+import uz.mohirdev.lesson.service.FileStorageService;
+
+@RestController
+@RequestMapping("/api")
 public class FileStorageResource {
+
+    private final FileStorageService fileStorageService;
+
+    public FileStorageResource(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity upload(@RequestParam("file")MultipartFile multipartFile){
+        FileStorage fileStorage = fileStorageService.save(multipartFile);
+        return ResponseEntity.ok(fileStorage);
+    }
 }
