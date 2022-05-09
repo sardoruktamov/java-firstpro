dccb66754884f2e55c9ec99b562838e0c9566684
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-09 21:48:53 +0500

    PostResource

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/PostResource.java
@@ -0,0 +1,27 @@
+package uz.mohirdev.lesson.web.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.mohirdev.lesson.model.Post;
+import uz.mohirdev.lesson.service.PostService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class PostResource {
+
+    private final PostService postService;
+
+    public PostResource(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity getAll(){
+        List<Post> resoult = postService.findAll();
+        return ResponseEntity.ok(resoult);
+    }
+}
