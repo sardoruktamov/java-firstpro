d6b05d967b540662d074f2f5e46cbb16836bc92c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-09 21:48:33 +0500

    RestTemplate

--- mohirdev/src/main/java/uz/mohirdev/lesson/MohirdevApplication.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/MohirdevApplication.java
@@ -2,12 +2,19 @@ package uz.mohirdev.lesson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MohirdevApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MohirdevApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
