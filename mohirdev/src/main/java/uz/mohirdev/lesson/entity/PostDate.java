9e16d46b2ba7fd272adf088c9bca05a1e3453d2c
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-10 21:36:16 +0500

    PostDate add

--- mohirdev/src/main/java/uz/mohirdev/lesson/entity/PostDate.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/PostDate.java
@@ -7,5 +7,51 @@ import java.io.Serializable;
 
 public class PostDate implements Serializable {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long postId;
+    private Long userId;
+    private String title;
+    private String body;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
