ccdb18f832b5abcbf2b381b48aa2da6ba567575d
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-10 21:23:06 +0500

    add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/PostDate.java
@@ -0,0 +1,11 @@
+package uz.mohirdev.lesson.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class PostDate implements Serializable {
+
+    
+}
