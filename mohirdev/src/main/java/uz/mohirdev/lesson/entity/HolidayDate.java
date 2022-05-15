d32453f6a28b182f1d47ca462d05a1b3ad44a077
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-15 22:23:13 +0500

    HolidayDate

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/HolidayDate.java
@@ -0,0 +1,106 @@
+package uz.mohirdev.lesson.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.List;
+
+@Entity
+public class HolidayDate implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant date;
+    private String localName;
+    private String name;
+    private String countryCode;
+    private Boolean fixed ;
+    private Boolean global;
+    private List counties;
+    private String launchYear;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public Boolean getFixed() {
+        return fixed;
+    }
+
+    public void setFixed(Boolean fixed) {
+        this.fixed = fixed;
+    }
+
+    public Boolean getGlobal() {
+        return global;
+    }
+
+    public void setGlobal(Boolean global) {
+        this.global = global;
+    }
+
+    public List getCounties() {
+        return counties;
+    }
+
+    public void setCounties(List counties) {
+        this.counties = counties;
+    }
+
+    public String getLaunchYear() {
+        return launchYear;
+    }
+
+    public void setLaunchYear(String launchYear) {
+        this.launchYear = launchYear;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
