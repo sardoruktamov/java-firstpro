d6c4e1c3901eaad8fbc483c8595b530133bad480
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-16 22:08:57 +0500

    change

--- mohirdev/src/main/java/uz/mohirdev/lesson/service/HolidayDateService.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/service/HolidayDateService.java
@@ -1,7 +1,13 @@
 package uz.mohirdev.lesson.service;
 
 import org.springframework.stereotype.Service;
+import uz.mohirdev.lesson.repository.HolidayDateRepository;
 
 @Service
 public class HolidayDateService {
+    private final HolidayDateRepository holidayDateRepository;
+
+    public HolidayDateService(HolidayDateRepository holidayDateRepository) {
+        this.holidayDateRepository = holidayDateRepository;
+    }
 }
