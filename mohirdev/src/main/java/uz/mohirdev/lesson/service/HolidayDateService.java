c0cb3eb3231d0b54bfd3d22aca51db776f786b86
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-05-18 22:28:11 +0500

    saveAll

--- mohirdev/src/main/java/uz/mohirdev/lesson/service/HolidayDateService.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/service/HolidayDateService.java
@@ -2,7 +2,12 @@ package uz.mohirdev.lesson.service;
 
 import org.springframework.stereotype.Service;
 import uz.mohirdev.lesson.entity.HolidayDate;
+import uz.mohirdev.lesson.model.Holiday;
 import uz.mohirdev.lesson.repository.HolidayDateRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class HolidayDateService {
@@ -16,5 +21,21 @@ public class HolidayDateService {
         return holidayDateRepository.save(holidayDate);
     }
 
-    public List
+    public List<HolidayDate> saveAll(Holiday[] holidays){
+        List<HolidayDate> holidayDateList = new ArrayList<>();
+        for (Holiday holiday:holidays) {
+            HolidayDate holidayDate = new HolidayDate();
+            holidayDate.setDate(holiday.getDate());
+            holidayDate.setLocalName(holiday.getLocalName());
+            holidayDate.setName(holiday.getName());
+            holidayDate.setCountryCode(holiday.getCountryCode());
+            holidayDate.setFixed(holiday.getFixed());
+            holidayDate.setGlobal(holiday.getGlobal());
+            holidayDate.setCounties(holiday.getCounties());
+            holidayDate.setLaunchYear(holiday.getLocalName());
+            holidayDate.setType(holiday.getType());
+            holidayDateList.add(holidayDate);
+        }
+        return holidayDateRepository.saveAll(holidayDateList);
+    }
 }
