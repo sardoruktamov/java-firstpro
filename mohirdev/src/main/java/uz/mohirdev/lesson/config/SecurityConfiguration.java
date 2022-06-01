536f647c171e55f2ed8ad611c8a766c6b3ed3b6d
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-01 19:46:35 +0500

    1-security qadam tugadi

--- mohirdev/src/main/java/uz/mohirdev/lesson/config/SecurityConfiguration.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package uz.mohirdev.lesson.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 }
