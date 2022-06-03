7d6349b2708ad84cf4aa06dcbe0ea96ba4019710
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-03 10:56:17 +0500

    user table change

--- mohirdev/src/main/java/uz/mohirdev/lesson/entity/User.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/User.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "mohirdev_user") //User nomli keyword borligi uchun jadval nomi ozgartirdik
@@ -15,6 +17,103 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull                    // javani o`zi 0 qiymat kelsa bazagacho o'tkazmasdan qaytaradi
+    @Size(min = 5, max = 15)
+    @Column(length = 15, unique = true, nullable = false)
+    private String login;
 
+    @NotNull
+    @Size(min = 20, max = 60)
+    @Column(length = 60, unique = true, nullable = false)
+    private String password;
 
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private Boolean activated;
+
+    private String langKey;  // site language
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {@JoinColumn(name = "uesr_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_name", referencedColumnName = "name")}
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActivated() {
+        return activated;
+    }
+
+    public void setActivated(Boolean activated) {
+        this.activated = activated;
+    }
+
+    public String getLangKey() {
+        return langKey;
+    }
+
+    public void setLangKey(String langKey) {
+        this.langKey = langKey;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
