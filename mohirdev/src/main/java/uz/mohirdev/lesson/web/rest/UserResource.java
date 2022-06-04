46153becaaad8ca017475e7c139cf86b5e2f7944
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-04 21:56:22 +0500

    UserResource add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/UserResource.java
@@ -0,0 +1,41 @@
+package uz.mohirdev.lesson.web.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.mohirdev.lesson.entity.User;
+import uz.mohirdev.lesson.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity create(@RequestBody User user){
+
+        // royxatdan o'tishda avvaldan login bo'lsa TRUE qaytaradi va biz ERROR qaytaramiz
+        if (userService.existsByLogin(user.getLogin())){
+            return new ResponseEntity("Bu Login Mavjud!", HttpStatus.BAD_REQUEST);
+        }
+
+        if (checkPasswordLength(user.getPassword())){
+            return new ResponseEntity("Parol 4 tadan kam!", HttpStatus.BAD_REQUEST);
+        }
+
+        User result = userService.save(user);
+        return ResponseEntity.ok(result);
+    }
+
+    private Boolean checkPasswordLength(String password){
+        return password.length() <= 4;
+    }
+}
