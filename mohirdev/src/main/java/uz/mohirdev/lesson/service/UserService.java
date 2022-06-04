d7d24e7bf77b5af980cf12b805f39d65dc217606
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-04 22:35:28 +0500

    PasswordEncoder add

--- mohirdev/src/main/java/uz/mohirdev/lesson/service/UserService.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/service/UserService.java
@@ -1,5 +1,6 @@
 package uz.mohirdev.lesson.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import uz.mohirdev.lesson.entity.User;
 import uz.mohirdev.lesson.repository.UserRepository;
@@ -8,12 +9,15 @@ import uz.mohirdev.lesson.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
