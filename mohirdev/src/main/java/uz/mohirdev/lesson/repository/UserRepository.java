24514abe29f5da5bbd36406bbd3edcafe04bdc7b
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-04 21:55:59 +0500

    UserRepository add

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package uz.mohirdev.lesson.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.mohirdev.lesson.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByLogin(String login);
+}
