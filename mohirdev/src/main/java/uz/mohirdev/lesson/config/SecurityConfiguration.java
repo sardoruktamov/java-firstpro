9d250ae3eb9e6a6584d88159dea50ecd097b9527
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-05 13:32:55 +0500

    userDetailsService add

--- mohirdev/src/main/java/uz/mohirdev/lesson/config/SecurityConfiguration.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/config/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package uz.mohirdev.lesson.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,13 +15,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfiguration(@Lazy UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()   //malumotlar bilan ishlash uhcun
-                .withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN")
-                .and()
-                .withUser("user").password(passwordEncoder().encode("1234")).roles("USER");
+                .userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+//                .inMemoryAuthentication()   //malumotlar bilan ishlash uhcun
+//                .withUser("admin").password(passwordEncoder().encode("1234")).roles("ADMIN")
+//                .and()
+//                .withUser("user").password(passwordEncoder().encode("1234")).roles("USER");
 
     }
 
