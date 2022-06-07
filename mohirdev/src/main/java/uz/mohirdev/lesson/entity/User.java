1b40ddab9087e1ffa5e5faf9fbb29f0a38b8929d
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-07 12:20:44 +0500

    ADMIN ORQALI KIRIB MALUMOT OLISH ISHLADI

--- mohirdev/src/main/java/uz/mohirdev/lesson/entity/User.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/entity/User.java
@@ -37,7 +37,7 @@ public class User implements Serializable {
 
     private String langKey;  // site language
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
             joinColumns = {@JoinColumn(name = "uesr_id", referencedColumnName = "id")},
