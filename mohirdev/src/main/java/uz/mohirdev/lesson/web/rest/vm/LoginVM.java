c15b29d2817a0f720de127ff757b3c74a820a723
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-10 13:52:59 +0500

    JWT token 8-qadam

---
+++ mohirdev/src/main/java/uz/mohirdev/lesson/web/rest/vm/LoginVM.java
@@ -0,0 +1,24 @@
+package uz.mohirdev.lesson.web.rest.vm;
+
+// JWT token 8-qadam
+public class LoginVM {
+
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
