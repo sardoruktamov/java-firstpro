0f4ee865afd8dad38461c791c48b63520d42aebb
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-10 13:43:27 +0500

    JWTtoken 6-qadam

--- mohirdev/src/main/java/uz/mohirdev/lesson/security/JwtProvider.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/security/JwtProvider.java
@@ -4,6 +4,9 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import uz.mohirdev.lesson.entity.Role;
 
@@ -47,4 +50,9 @@ public class JwtProvider {
 
     }
 
+    // parolni encode qilish (JWTtoken 6-qadam)
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
