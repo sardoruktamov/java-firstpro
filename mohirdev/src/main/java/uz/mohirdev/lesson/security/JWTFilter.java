08c815f0714d141717869cabb72e7d6a07a8e4f2
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-06-19 11:25:37 +0500

    Update JwtProvider

--- mohirdev/src/main/java/uz/mohirdev/lesson/security/JWTFilter.java
+++ mohirdev/src/main/java/uz/mohirdev/lesson/security/JWTFilter.java
@@ -1,5 +1,8 @@
 package uz.mohirdev.lesson.security;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -12,8 +15,34 @@ import java.io.IOException;
 public class JWTFilter extends GenericFilterBean {
     // klent tomonidan kelayotgan va ketayotgan barcha malumot birinchi bolib doFilterga keladi
 
+    private final JwtProvider jwtProvider;
+
+    public JWTFilter(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+        // klent tomonidan kelayotgan barcha sorovlarni ushlab olish
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        //kelayotgan Datani ichidan tokenni olamiz
+        String jwt = resolveToket(httpServletRequest);
+        if (StringUtils.hasText(jwt) && this.jwtProvider.validateToken(jwt)){
+            Authentication authentication = this.jwtProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request,response);
+    }
+
+    private String resolveToket(HttpServletRequest httpServletRequest) {
+        //headerni ushlab olib tokenni headersga solib jonatamiz
+        String bearerToken = httpServletRequest.getHeader("Authorization");
+        //olingan tokenni Null va boshqa holatlarga tekshirib koramiz
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            // bearertokenni 7(Bearer ) ta belgisini kesib qolganini yuboramiz
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
